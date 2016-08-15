a1b6a3db363eaca5a7dd13bb73534d326ad07d01
tianxq1 <tianxq1@yonyou.com>
2016-08-15 15:24:03 +0800

    同步项目上的代码

--- website/eccm2/src/components/common/gridComp/gridComp.js
+++ website/eccm2/src/components/common/gridComp/gridComp.js
@@ -1,4 +1,94 @@
-$.fn.grid.gridComp.prototype.createHeader = function() {
+var gridCompProto = $.fn.grid.gridComp.prototype;
+gridCompProto.initOptionsTree = function() {
+
+}
+
+gridCompProto.initGrid  = function() {
+		if(!this.options.columns || this.options.columns.length == 0){
+			return;
+		}
+		var oThis = this;
+		this.initOptions();
+		this.initVariable();
+		this.initWidthVariable();
+		this.initGridCompColumn();
+		this.initDataSource();
+		this.createDivs();
+		// 去掉
+		//this.inte = setInterval(function(){oThis.setIntervalFun.call(oThis)}, 300);
+	},
+
+gridCompProto.createContentLeft = function() {
+		var oThis = this,htmlStr = "",left = 0,hStr;
+		if(this.options.multiSelect){
+			htmlStr += '<div class="u-grid-content-left" id="' + this.options.id + '_content_multiSelect" style="width:' + this.multiSelectWidth + 'px;' + hStr + '">';
+			// 遍历生成所有行
+			if (this.dataSourceObj.rows) {
+				$.each(this.dataSourceObj.rows, function(i) {
+					htmlStr += oThis.createContentLeftMultiSelectRow(this);
+				});
+			}
+			htmlStr += '</div>';
+			left += this.multiSelectWidth;
+		}
+		if (this.options.showNumCol) {
+			htmlStr += '<div class="u-grid-content-left" id="' + this.options.id + '_content_numCol" style="width:' + this.numWidth + 'px;left:' + left + 'px;' + hStr + '">';
+			// 遍历生成所有行
+			if (this.dataSourceObj.rows) {
+				this.treeNumCounter = 1;
+				$.each(this.dataSourceObj.rows, function(i, row) {
+					htmlStr += oThis.createContentLeftNumColRow(i, row.value);
+				});
+			}
+			htmlStr += '</div>';
+		}
+		return htmlStr;
+	}
+
+
+gridCompProto.createContentLeftNumColRow = function(index, row) {
+	if(this.options.showTree) {
+		if(!row.parentId) {
+			return '<div style="width:' + this.numWidth + 'px;" class="u-grid-content-num topLevel">' + (this.treeNumCounter++) + '</div>';
+		} else {
+			return '<div style="width:' + this.numWidth + 'px;display:none;" class="u-grid-content-num"></div>';;
+		}
+	}
+	var htmlStr = '<div style="width:' + this.numWidth + 'px;" class="u-grid-content-num">' + (index+1) + '</div>';
+	return htmlStr;
+}
+var oriResetNumCol = gridCompProto.resetNumCol;
+gridCompProto.resetNumCol = function() {
+	if(!this.options.showTree) {
+		return oriResetNumCol.call(this);
+	}
+	var numCols = $('#' + this.options.id + '_content_numCol >.u-grid-content-num');
+  var num = 1;
+	$.each(numCols,function(i){
+		if(u.hasClass(this, 'topLevel')) {
+			this.innerHTML = num++;
+		}
+	});
+}
+
+var oriClickFunTree = gridCompProto.clickFunTree;
+gridCompProto.clickFunTree = function(e) {
+	oriClickFunTree.call(this,e);
+	var trs = $('#' + this.options.id + '_content_table > tbody > tr');
+	var numCols = $('#' + this.options.id + '_content_numCol >.u-grid-content-num');
+
+	$.each(trs,function(i, tr){
+		var visible = $(tr).is(":visible"), $num = $(numCols[i])
+		if(visible) {
+			$num.show();
+		} else {
+			$num.hide();
+		}
+	})
+
+}
+
+gridCompProto.createHeader = function() {
 	var wrapStr = '',headerShowStr = '';
 	if(!this.options.showHeader)
 		headerShowStr = 'style="display:none;"';
@@ -26,7 +116,7 @@ $.fn.grid.gridComp.prototype.createHeader = function() {
 			if(gridBrowser.isIE8){
 				//htmlStr += '<div class="u-grid-header-multi-select" style="width:' + this.multiWidth + 'px;"><input class="u-grid-multi-input"   type="checkbox" id="' + this.options.id + '_header_multi_input"></div>'
 				htmlStr += '<div class="u-grid-header-multi-select" style="width:' + this.multiWidth + 'px;"><span class="u-grid-checkbox-outline" id="' + this.options.id + '_header_multi_input"><span class="u-grid-checkbox-tick-outline"></span></span></div>'
-				
+
 			}else{
 				//htmlStr += '<div class="u-grid-header-multi-select  checkbox check-success" style="width:' + this.multiWidth + 'px;"><input  class="u-grid-multi-input"  type="checkbox" id="' + this.options.id + '_header_multi_input"><label for="' + this.options.id + '_header_multi_input"></label></div>'
 				htmlStr += '<div class="u-grid-header-multi-select  checkbox check-success" style="width:' + this.multiWidth + 'px;"><span class="u-grid-checkbox-outline" id="' + this.options.id + '_header_multi_input"><span class="u-grid-checkbox-tick-outline"></span></span></div>'
@@ -44,92 +134,3 @@ $.fn.grid.gridComp.prototype.createHeader = function() {
 	htmlStr += '</div>';
 	return htmlStr;
 }
-
-u.on(window, 'load', function() {
-	viewModel = {
-		dataTable: new u.DataTable({
-			meta: {
-				"name": "",
-				"time": "",
-				"distance": "",
-				"currency": ""
-			}
-		}, this),
-	};
-	var app = u.createApp({
-	    el: '#girdComp',
-	    model: viewModel
-	});
-	var data = {
-		"pageIndex": 1,
-		"pageSize": 10,
-		"rows": [{
-			"status": "nrm",
-			"data": {
-				"name": "赵四",
-				"time": "2016-05-16",
-				"distance": "25",
-				"currency": {
-					"value": "200.00",
-					"meta": {
-						"precision": "2",
-						"max": "3000",
-						"min": "0",
-						"curSymbol": "$"
-					}
-				}
-			}
-		}, {
-			"status": "nrm",
-			"data": {
-				"name": "王一",
-				"time": "2016-05-12",
-				"distance": "25",
-				"currency": {
-					"value": "200.00",
-					"meta": {
-						"precision": "2",
-						"max": "3000",
-						"min": "0",
-						"curSymbol": "$"
-					}
-				}
-			}
-		}, {
-			"status": "nrm",
-			"data": {
-				"name": "李三",
-				"time": "2016-11-16",
-				"distance": "50",
-				"currency": {
-					"value": "300.00",
-					"meta": {
-						"precision": "2",
-						"max": "3000",
-						"min": "0",
-						"curSymbol": "$"
-					}
-				}
-			}
-		}, {
-			"status": "nrm",
-			"data": {
-				"name": "彰武",
-				"time": "2012-05-16",
-				"distance": "50",
-				"currency": {
-					"value": "300.00",
-					"meta": {
-						"precision": "2",
-						"max": "3000",
-						"min": "0",
-						"curSymbol": "$"
-					}
-				}
-			}
-		}]
-	}
-	viewModel.dataTable.removeAllRows();
-	viewModel.dataTable.setData(data);
-
-})
